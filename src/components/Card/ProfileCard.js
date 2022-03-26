5254e6ef9602896518f040a3b26ae35ae81849d8
touseefcodes <touseef69r@gmail.com>
2022-03-26 18:34:32 +0500

    chore: responsive card

--- src/components/Card/ProfileCard.js
+++ src/components/Card/ProfileCard.js
@@ -5,24 +5,27 @@ import me from "../../data/images/mee.jpg";
 const ProfileCard = () => {
   return (
     <main>
-      <center className="max-w-5xl h-72 grid grid-cols-12 text-left bg-secondary-light-color rounded-lg overflow-hidden">
-        <div className="col-span-4">
+      <center className="text-cemter w-full overflow-hidden rounded-lg bg-secondary-light-color md:flex md:h-[288px] md:items-center md:justify-center md:text-left">
+        <figure className="mx-auto my-4 h-24 w-24 sm:h-28 sm:w-28 md:mx-0 md:my-0 md:h-[288px] md:w-1/3">
           <Image
             src={me}
             alt="me"
-            width={620}
-            height={600}
-            className="w-full h-full rounded-l-lg object-center object-cover"
+            className="rounded-full object-cover md:rounded-none md:rounded-l-lg"
             loading="lazy"
           />
-        </div>
-        <article className="col-start-5 col-end-13 px-8 py-10 text-lg font-medium">
-          Touseef is a frontend developer specializing in React.js, NextJS,
-          Tailwindcss, and SCSS. His abilities go beyond just coding as he's a
-          quick learner and has a large appetite for knowledge. He has good
-          leadership skills and great communication skills as well. His role is
-          to write and style the front-end components that meet the requirements
-          of our mocks and fulfill our user stories.
+        </figure>
+        <article className="px-4 py-4 text-sm font-medium leading-relaxed sm:py-5 sm:text-base md:w-2/3 md:py-0 md:px-8 md:text-lg md:tracking-wide">
+          <h2 className="mb-2 text-lg font-bold md:text-2xl">
+            Touseef ibn Khaleel
+          </h2>
+          <p>
+            Touseef is a frontend developer specializing in React.js, NextJS,
+            Tailwindcss, and SCSS. His abilities go beyond just coding as he's a
+            quick learner and has a large appetite for knowledge. He has good
+            leadership skills and great communication skills as well. His role
+            is to write and style the front-end components that meet the
+            requirements of our mocks and fulfill our user stories.
+          </p>
         </article>
       </center>
     </main>
